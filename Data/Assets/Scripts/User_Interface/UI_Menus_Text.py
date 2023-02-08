bbf66d0910784b0f269f1a69a112c83553f4a60a
Alexander <95552627+Pawsanie@users.noreply.github.com>
2023-02-08 21:07:59 +0300

    Update 0.0.68: MenuText class constructor
    Update MenuText  class constructor

--- Data/Assets/Scripts/User_Interface/UI_Menus_Text.py
+++ Data/Assets/Scripts/User_Interface/UI_Menus_Text.py
@@ -1,6 +1,7 @@
-from pygame import Surface
+from pygame import Surface, font
 
-from ..Assets_load import json_load
+from ..Assets_load import json_load, font_load
+font.init()
 """
 Contents code for menus text keeper.
 """
@@ -11,13 +12,40 @@ class MenuText:
     Keep menus text.
     """
     def __init__(self, *, background_surface: Surface, menu_name: str, menu_text: str,
-                 language_flag: str, menu_text_localization_dict: dict[str]):
+                 language_flag: str, menu_text_localization_dict: dict[str], menu_text_font: str or None,
+                 menu_text_color: str, menu_text_coordinates: dict[str, int]):
+        """
+        :param background_surface: pygame.Surface of background.
+        :type background_surface: Surface
+        :param menu_name: The name of the menu the text is for.
+        :type menu_name: str
+        :param menu_text: Text of the menu.
+        :type menu_text: str
+        :param language_flag: String with language flag.
+        :type language_flag: str
+        :param menu_text_localization_dict: Dictionary with language flags as keys and localization text as values.
+        :type menu_text_localization_dict: dict[str]
+        :param menu_text_font: Font name for font asset load.
+        :type menu_text_font: str | None
+        :param menu_text_color: Text color.
+        :type menu_text_color: str
+        :param menu_text_coordinates: Dictionary with str(x|y) as key and int as value.
+        :type menu_text_coordinates: dict[str, int]
+        """
         # Arguments processing:
         self.background_surface: Surface = background_surface
         self.menu_name: str = menu_name
         self.language_flag: str = language_flag
         self.menu_text: str = menu_text
         self.localisation_menu_text: dict[str] = menu_text_localization_dict
+        self.menu_text_coordinates_x = menu_text_coordinates['x']
+        self.menu_text_coordinates_y = menu_text_coordinates['y']
+        self.font_size: int = 0
+        self.text_color: str = menu_text_color
+        self.text_font: str or None = menu_text_font
+        if self.text_font is not None:
+            self.font_name: str = self.text_font
+            self.set_button_font: font.Font = font_load(font_name=self.font_name, font_size=self.font_size)
 
     def get_text(self) -> str:
         """
@@ -85,7 +113,10 @@ def menus_text_generator(language_flag: str, background_surface: Surface) -> dic
                 menu_name=text_type,
                 menu_text=menu_text,
                 language_flag=language_flag,
-                menu_text_localization_dict=menu_text_localization
+                menu_text_localization_dict=menu_text_localization,
+                menu_text_font=ui_menus_texts_json['font'],
+                menu_text_color=ui_menus_texts_json['color'],
+                menu_text_coordinates=ui_menus_texts_json['coordinates']
             )})
 
         result.update({file_name: ui_menus_texts})
