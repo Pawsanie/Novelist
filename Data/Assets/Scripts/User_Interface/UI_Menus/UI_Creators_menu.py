5a7005688e4c3e98f8b380ecf2e5a0a22f2d5411
Alexander <95552627+Pawsanie@users.noreply.github.com>
2023-02-26 17:02:47 +0300

    Update 0.0.79: CreatorsMenu
    Update CreatorsMenu.

--- Data/Assets/Scripts/User_Interface/UI_Menus/UI_Creators_menu.py
+++ Data/Assets/Scripts/User_Interface/UI_Menus/UI_Creators_menu.py
@@ -1,3 +1,5 @@
+from pygame import K_e, K_TAB, KEYDOWN, K_ESCAPE
+
 from ..UI_Base_menu import BaseMenu
 """
 Contains creators menu code.
@@ -12,8 +14,20 @@ class CreatorsMenu(BaseMenu):
         super(CreatorsMenu, self).__init__()
 
     def creators_menu_back(self):
+        """
+        Back to start menu.
+        """
         self.interface_controller.creator_menu = False
         self.interface_controller.start_menu_status = True
+
+    def key_bord_exit_menu_key_down(self, event):
+        """
+        Interface interaction in in-game setting menu.
+        :param event: pygame.event from main_loop.
+        """
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE or event.key == K_TAB or event.key == K_e:
+                self.creators_menu_back()
 
     def creators_menu_input_mouse(self, event):
         """
@@ -31,5 +45,8 @@ class CreatorsMenu(BaseMenu):
         Save menu conveyor:
         :param event: pygame.event from main_loop.
         """
+        # Button game menu ui status:
         self.creators_menu_input_mouse(event)
+        # Button game menu key bord status:
+        self.key_bord_exit_menu_key_down(event)
         self.input_wait_ready()
