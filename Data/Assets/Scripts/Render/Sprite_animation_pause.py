fefe2d1511ef1a6800251ba5b82c95fdd43090d3
Alexander <pawsanie@gmail.com>
2023-10-24 18:18:08 +0600

    Update 0.3.26: Difference in pause between animations
    SpriteAnimationPause:
    Now the class collects information about the sprites that are used in the scene and is able to perform animations asynchronously.

--- Data/Assets/Scripts/Render/Sprite_animation_pause.py
+++ Data/Assets/Scripts/Render/Sprite_animation_pause.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from pygame import time
 
 from ..Universal_computing.Pattern_Singleton import SingletonPattern
@@ -11,11 +13,8 @@ class SpriteAnimationPause(SingletonPattern):
     Decorator class which hold self time for animation pause animation tipe switch.
     """
     def __init__(self):
-        self.frame_time: int = time.get_ticks()
-        self.animation_skip: bool = False
-        self.animation_skip_time: int = 4
-
         self.scene_name: str = ""
+        self.sprite_collection: dict = {}
 
     def __call__(self, func):
         """
@@ -28,24 +27,36 @@ class SpriteAnimationPause(SingletonPattern):
 
             if self.scene_name != scene_name:
                 self.scene_name = scene_name
-                self.animation_skip = False
+                self.sprite_collection.clear()
                 return func(*args, **kwargs)
+
+            if any((
+                    decorated_self.name,
+                    str(decorated_self)
+            )) not in self.sprite_collection.keys():
+                self.update_scene_sprites(decorated_self)
 
             if decorated_self.statick_frame_key is None:
                 sprite_sheet_len: int = len(
-                            decorated_self.sprite_sheet[decorated_self.animation_name]
+                            decorated_self.sprite_sheet
+                            [decorated_self.animation_name]
                             [decorated_self.frames]
                         )
+                if decorated_self.name is not None:
+                    sprite_name: str = decorated_self.name
+                else:
+                    sprite_name: str = str(decorated_self)
+                sprite: dict = self.sprite_collection[sprite_name]
 
-                if decorated_self.last_frame_number == sprite_sheet_len - 1:
-                    self.animation_skip = True
-                    decorated_self.last_frame_number = -1
+                if sprite["sprite"].last_frame_number == sprite_sheet_len - 1:
+                    sprite["animation_skip"]: bool = True
+                    sprite["sprite"].last_frame_number = -1
 
-                if self.animation_skip is True:
-                    decorated_self.last_frame_number = -1
-                    if time.get_ticks() / 1000 >= self.frame_time / 1000 + self.animation_skip_time:
-                        self.animation_skip = False
-                        self.frame_time = time.get_ticks()
+                if sprite["animation_skip"] is True:
+                    sprite["sprite"].last_frame_number = -1
+                    if time.get_ticks() / 1000 >= sprite["frame_time"] / 1000 + sprite["pause_time"]:
+                        sprite["animation_skip"]: bool = False
+                        sprite["frame_time"]: int = time.get_ticks()
 
             return func(*args, **kwargs)
         return decorated
@@ -57,3 +68,21 @@ class SpriteAnimationPause(SingletonPattern):
         """
         from ..Application_layer.Scene_Validator import SceneValidator
         return SceneValidator().scene
+
+    def update_scene_sprites(self, sprite):
+        """
+        :param sprite: Sprite for "sprite_collection".
+        :type sprite: Sprite
+        """
+        if sprite.name is not None:
+            sprite_name: str = sprite.name
+        else:
+            sprite_name: str = str(sprite)
+        self.sprite_collection.setdefault(
+            sprite_name, {
+                "sprite": sprite,
+                "animation_skip": False,
+                "pause_time": randint(2, 5),
+                "frame_time": time.get_ticks()
+                }
+        )
