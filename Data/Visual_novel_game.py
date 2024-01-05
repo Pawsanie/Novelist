b46e859f3cb2fee6ee179ef5298d9e3126317a91
Alexander <pawsanie@gmail.com>
2024-01-05 14:34:33 +0600

    Update 0.3.28: Simplified the code
    Simplified the entry point code.

--- Data/Visual_novel_game.py
+++ Data/Visual_novel_game.py
@@ -11,13 +11,19 @@ from Assets.Scripts.Logging_Config import logging_config, text_for_logging
 Contains app shell code.
 """
 
-app_name: str = "Visual Novel"
-
 
 def run():
     """
     Initialization.
     """
+    app_name: str = "Visual Novel"
+
+    icon_set: dict[str] = {
+        "Windows": "win_icon",
+        "Mac_OS": "mac_icon",
+        "linux": "nix_icon"
+    }
+
     # Set game settings:
     start_settings: SettingsKeeper = SettingsKeeper()
     type_of_system: str = start_settings.system_type
@@ -27,27 +33,12 @@ def run():
     ])
     # Application name in window:
     display.set_caption(app_name)
-    # Window settings:
-    if type_of_system == 'Windows':
-        display.set_icon(image_load(
-            art_name='win_icon',
-            file_format='png',
-            asset_type=path_to_icons
-        ))
-    # Mac settings:
-    if type_of_system == 'Mac_OS':
-        display.set_icon(image_load(
-            art_name='mac_icon',
-            file_format='png',
-            asset_type=path_to_icons
-        ))
-    # Unix settings:
-    if type_of_system == 'linux':
-        display.set_icon(image_load(
-            art_name='nix_icon',
-            file_format='png',
-            asset_type=path_to_icons
-        ))
+    # Icon settings:
+    display.set_icon(image_load(
+        art_name=icon_set[type_of_system],
+        file_format='png',
+        asset_type=path_to_icons
+    ))
     # Start game:
     gameplay: GameMaster = GameMaster()
     gameplay()
