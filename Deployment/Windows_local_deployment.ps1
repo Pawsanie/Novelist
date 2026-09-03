db791dc419c0335d5fd146a99a9849597d5ee9be
Alexander <pawsanie@gmail.com>
2026-09-03 20:42:00 +0500

    Update 0.3.149 - Deployment - PowerShell: Windows deployment update

--- Deployment/Windows_local_deployment.ps1
+++ Deployment/Windows_local_deployment.ps1
@@ -4,7 +4,8 @@
 
 # Path settings:
 $ScriptDir = Split-Path `
-                -Parent $MyInvocation.MyCommand.Path
+    -Parent $MyInvocation.MyCommand.Path
+
 $TargetDir = @(
     $(
         Split-Path `
@@ -14,7 +15,16 @@ $TargetDir = @(
     "Python"
 ) `
     -join "\"
-$PythonExe = "$TargetDir\python-3.10.11-amd64.exe"
+
+# Python settings:
+$PythonVersionFull = "3.13.15"
+$PythonVersionShort = "3.13"
+$PythonVersionFolder = "313"
+
+$PythonExe = "$TargetDir\python-$PythonVersionFull-amd64.exe"
+
+$PythonSigner = "Python Software Foundation"
+$PythonFileSHA256 = "edec09c4853aeae9ac36efb8c9f95b6b8e2fee65eee56d9767a8b7c69c574403"
 
 <#
 .SYNOPSIS
@@ -26,16 +36,36 @@ function Test-Python-Existence {
 
     foreach (
         $Path in @(
-            "$Env:ProgramFiles\Python310\python.exe",
-            "$Env:LOCALAPPDATA\Programs\Python\Python310\python.exe",
-            "$Env:LOCALAPPDATA\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe"
+            "$Env:ProgramFiles\Python$($PythonVersionFolder)\python.exe",
+            "$Env:LOCALAPPDATA\Programs\Python\Python$($PythonVersionFolder)\python.exe",
+            "$Env:LOCALAPPDATA\Microsoft\WindowsApps\PythonSoftwareFoundation.$($PythonVersionShort)_qbz5n2kfra8p0\python.exe",
+            "$TargetDir\python.exe"
         )
     ) {
 
         if (Test-Path $Path) {
 
-            $Instance = $Path
-            break
+            try {
+
+                if (
+                    (
+                        & $Path `
+                            -V
+                    ) `
+                    -eq "Python $($PythonVersionFull)"
+                ) {
+
+                    $Instance = $Path
+                    break
+
+                }
+
+            }
+            catch {
+
+                continue
+
+            }
 
         }
 
@@ -51,13 +81,50 @@ Download Python from the official website and install it.
 #>
 function Get-Python {
 
-    Write-Host `
-        "Getting Python from the official website..." `
-        -ForegroundColor Blue
+    if (
+        (Test-Path $PythonExe) `
+        -and (
+                (
+                    Get-AuthenticodeSignature `
+                        -FilePath $PythonExe
+                ).Status `
+                    -eq "Valid"
+            ) `
+        -and (
+                (
+                    Get-AuthenticodeSignature `
+                        -FilePath $PythonExe
+                ).SignerCertificate.Subject `
+                    -like "*$PythonSigner*"
+            ) `
+        -and (
+                (
+                    Get-FileHash `
+                        $PythonExe `
+                        -Algorithm SHA256
+                ).Hash.ToUpper() `
+                    -eq $PythonFileSHA256.ToUpper()
+        )
+    ) {
 
-    Invoke-WebRequest `
-        -Uri "https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe" `
-        -OutFile $PythonExe
+        Write-Host `
+            "Python.$PythonVersionFull installation file already exists...`n" `
+            "File path: $PythonExe`n" `
+            "No download required." `
+            -ForegroundColor Blue
+
+    }
+    else {
+
+        Write-Host `
+            "Getting Python.$PythonVersionFull from the official website..." `
+            -ForegroundColor Blue
+
+        Invoke-WebRequest `
+            -Uri "https://www.python.org/ftp/python/$PythonVersionFull/python-$PythonVersionFull-amd64.exe" `
+            -OutFile $PythonExe
+
+    }
 
     Write-Host `
         "Deploying Python..." `
@@ -72,6 +139,7 @@ function Get-Python {
                 "InstallLauncherAllUsers=0",
                 "PrependPath=0",
                 "Include_launcher=0",
+                "Include_pip=1",
                 "TargetDir=$TargetDir"
             ) `
                 -join " "
@@ -83,9 +151,9 @@ function Get-Python {
 
 <#
 .SYNOPSIS
-Runs aaplication requirements deployment pipeline.
+Deploys a suitable local Python environment for Windows.
 #>
-function Main {
+function Install-Python {
 
     if (
         !(Test-Path $TargetDir)
@@ -103,7 +171,7 @@ function Main {
     ) {
 
         Write-Host `
-            "Creating a virtual environment from a local version of Python.3.10..." `
+            "Creating a virtual environment from a local version of Python.$PythonVersionShort..." `
             -ForegroundColor Blue
 
         & $PythonEntity `
@@ -114,11 +182,6 @@ function Main {
 
         Get-Python
 
-        & "$TargetDir\python.exe" `
-            -m venv `
-            --copies `
-            $TargetDir
-
     }
 
     try {
@@ -126,7 +189,28 @@ function Main {
         $Success = $true
 
         Write-Host `
-            "Get requirements libs..." `
+            "Creating a Python virtual environment..." `
+            -ForegroundColor Blue
+
+        try {
+
+            & "$TargetDir\python.exe" `
+                -m venv `
+                --copies `
+                $TargetDir
+
+        }
+        catch {
+
+            & "$TargetDir\Scripts\python.exe" `
+            -m venv `
+            --copies `
+            $TargetDir
+
+        }
+
+        Write-Host `
+            "Get Python requirements libs..." `
             -ForegroundColor Blue
 
         & "$TargetDir\Scripts\python.exe" `
@@ -141,16 +225,19 @@ function Main {
 
         Write-Host `
             "An error occurred during deployment...`n" `
-            "It looks like you already had Python 3.10.11 installed and uninstalled incorrectly.`n" `
+            "It looks like you already had Python $PythonVersionFull installed and uninstalled incorrectly.`n" `
             "Try restoring the version from the installation file:`n" `
-            "$TargetDir\python-3.10.11-amd64.exe`n" `
+            "$TargetDir\python-$PythonVersionFull-amd64.exe`n" `
             "After that, re-run this script to complete the local deployment." `
             -ForegroundColor Red
 
     }
+
     if ($Success) {
 
-        if (Test-Path $PythonExe) {
+        if (
+            (Test-Path $PythonExe)
+        ) {
 
              Write-Host `
                 "Removing the installation python.exe file..." `
@@ -166,6 +253,24 @@ function Main {
             -ForegroundColor Green
 
     }
+
+}
+
+<#
+.SYNOPSIS
+Runs aaplication requirements deployment pipeline.
+#>
+function Main {
+
+    Write-Host `
+        "Application requirements deployment script has been launched."`
+        -ForegroundColor Blue
+
+    Install-Python
+
+    Write-Host `
+        "Application requirements deployment scenario completed." `
+        -ForegroundColor Blue
 
 }
 
